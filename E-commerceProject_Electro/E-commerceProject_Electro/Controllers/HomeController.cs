796ed1aea37941c2fe052d53410e33aa1a9a1a36
mihaRik <rahimrt@code.edu.az>
2018-12-23 15:59:12 +0400

    timer hot deal begin to write

--- E-commerceProject_Electro/E-commerceProject_Electro/Controllers/HomeController.cs
+++ E-commerceProject_Electro/E-commerceProject_Electro/Controllers/HomeController.cs
@@ -42,6 +42,7 @@ namespace E_commerceProject_Electro.Controllers
                 })
                 .ToList()
                 .Find(x => x.Id == id);
+
             if (selectedProduct == null)
             {
                 return View("_ErrorPage");
@@ -50,9 +51,46 @@ namespace E_commerceProject_Electro.Controllers
             return View(selectedProduct);
         }
 
+        [HttpGet]
         public ActionResult Store()
         {
             ICollection<ProductToImages> products = _db.Products
+                .GroupJoin(_db.ProductImages, x => x.Id, y => y.ProductId, (x, y) => new ProductToImages()
+                {
+                    Id = x.Id,
+                    ProductName = x.ProductName,
+                    ProductPrice = x.ProductPrice,
+                    ProductDiscountValueInPercents = x.ProductDiscountValueInPercents,
+                    ProductAddDate = x.ProductAddDate,
+                    ProductImages = y,
+                })
+                .ToList();
+
+            return View(products);
+        }
+
+        [HttpPost]
+        public ActionResult Store(string categories, string searchTxt)
+        {
+            int categoryId = Convert.ToInt32(categories);
+
+            List<Product> filteredProducts;
+
+            if (categoryId == 0)
+            {
+                filteredProducts = _db.Products
+                    .Where(x => x.ProductName.Contains(searchTxt))
+                    .ToList();
+            }
+            else
+            {
+                filteredProducts = _db.Products
+                    .Where(x => x.CategoryId == categoryId
+                    && x.ProductName.Contains(searchTxt))
+                    .ToList();
+            }
+
+            List<ProductToImages> products = filteredProducts
                 .GroupJoin(_db.ProductImages, x => x.Id, y => y.ProductId, (x, y) => new ProductToImages()
                 {
                     Id = x.Id,
